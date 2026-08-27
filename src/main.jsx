23c532f097bc57f9988ea9f30fc2e5beca17da09
jvillanuevaleal2507-coder <jvillanuevaleal2507@gmail.com>
2026-08-26 20:21:09 -0500

    Usar texto casual al compartir selección

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { Analytics } from "@vercel/analytics/react";
 import { initDonatelloFavorites } from "./favorites.js";
+import { initDonatelloFavoriteSharing } from "./shareFavorites.js";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -14,3 +15,4 @@ createRoot(document.getElementById("root")).render(
 );
 
 initDonatelloFavorites();
+initDonatelloFavoriteSharing();
