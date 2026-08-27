408f18c24239a1510caf1e0139286ea9407054ba
jvillanuevaleal2507-coder <jvillanuevaleal2507@gmail.com>
2026-08-26 21:05:22 -0500

    Activar detalle desde favoritos

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from "./App.jsx";
 import { Analytics } from "@vercel/analytics/react";
 import { initDonatelloFavorites } from "./favorites.js";
 import { initDonatelloFavoriteSharing } from "./shareFavorites.js";
+import { initDonatelloFavoriteDetail } from "./favoritesDetail.js";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -16,3 +17,4 @@ createRoot(document.getElementById("root")).render(
 
 initDonatelloFavorites();
 initDonatelloFavoriteSharing();
+initDonatelloFavoriteDetail();
